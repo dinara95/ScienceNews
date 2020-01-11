e4bd6075e9baa69874132c8a6b50a7c817e7d50b
Dinara <dinara9599@gmail.com>
2020-01-11 14:57:41 +0600

    Initial Commit

---
+++ ScienceNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScienceNews
+//
+//  Created by Dinara Shadyarova on 1/11/20.
+//  Copyright © 2020 Dinara Shadyarova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
